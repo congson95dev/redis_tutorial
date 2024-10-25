cd262fb82f130c2b1942e4c16fa1b15edb96bab3
Thomas Nguyen <thomas.nguyen1@vmogroup.com>
2024-10-25 14:49:01 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # redis_tutorial
+
+Start virtual environment
+
+`sudo pip3 install virtualenv`
+
+`python3 -m venv venv`
+
+`source venv/bin/activate`
+
+Run
+
+`pip install redis`
+
+`docker run --name redis-server -p 6379:6379 -d redis`
+
+`python3 tutorial.py`
--- tutorial.py
+++ tutorial.py
@@ -6,11 +6,6 @@ redis_client = redis.Redis(host='localhost', port=6379, db=0)
 def get_data():
     return {"name": "Fudo"}
 
-# caching to redis with json format
-# check if data is cached in redis yet, if not, call to function get_data() and store the result as cache in redis
-# else, get the data from redis
-# this will so much faster than just calling to the function
-
 data = redis_client.get('data')
 if data is None:
     print("not found in redis, run function get_data()")
